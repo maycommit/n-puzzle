3f818da81f8da53bde7c7ff7df26ebf8478832fb
Maycon Pacheco <mayconjrpacheco@gmail.com>
2022-05-19 16:13:23 -0300

    Add more tests

--- test_puzzle.py
+++ test_puzzle.py
@@ -1,6 +1,6 @@
 from puzzle import Puzzle
 
-def test_puzzle():
+def test_puzzle_1():
     initial_state = [
         [4, 5, 1],
         [2, 3, 6],
@@ -15,8 +15,26 @@ def test_puzzle():
 
     puzzle = Puzzle()
     puzzle.resolve_with_breadth_first_search(initial_state, goal_state)
-    print(puzzle.state_count)
     assert puzzle.state_count == 2
 
+def test_puzzle_2():
+    initial_state = [
+        [4, 5, 1],
+        [2, 3, 6],
+        [0, 8, 7],
+    ]
+
+    goal_state = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 0],
+    ]
+
+    puzzle = Puzzle()
+    puzzle.resolve_with_breadth_first_search(initial_state, goal_state)
+    assert puzzle.state_count == 181440
+
 if __name__ == "__main__":
-    test_puzzle()
+    test_puzzle_1()
+    test_puzzle_2()
+    print("Everything passed")
