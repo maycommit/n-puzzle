42b532fba469012501c2aca2a0b4d0fded2ad78f
Maycon Pacheco <mayconjrpacheco@gmail.com>
2022-06-02 13:28:45 -0300

    Print last node

--- priority_queue_test.py
+++ priority_queue_test.py
@@ -6,15 +6,15 @@ class TestPriorityQueue(unittest.TestCase):
     def test_case_1(self):
         p_queue = PriorityQueue()
 
-        p_queue.put(Node([[111]], 1, cost=3))
+        p_queue.put(Node([[000]], 1, cost=3))
         p_queue.put(Node([[111]], 1, cost=0))
         p_queue.put(Node([[222]], 2, cost=5))
         p_queue.put(Node([[333]], 2, cost=1))
         p_queue.put(Node([[444]], 2, cost=4))
         p_queue.replace_priority(Node([[222]], 2, cost=3))
         p_queue.put(Node([[555]], 2, cost=1))
-        p_queue.put(Node([[555]], 2, cost=10))
-        p_queue.put(Node([[555]], 2, cost=6))
+        p_queue.put(Node([[666]], 2, cost=10))
+        p_queue.put(Node([[777]], 2, cost=6))
 
         result = []
         while not p_queue.empty():
